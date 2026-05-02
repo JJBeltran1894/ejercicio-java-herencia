fce1d54d4b5c71f7c94d284e613fb35657f23d36
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-05-01 23:05:51 -0500

    JJBA: Agregada la Clase Padre.java. Atributos, constructor, getters y setters

---
+++ src/com/krakedev/herencia/Padre.java
@@ -0,0 +1,39 @@
+package com.krakedev.herencia;
+
+public class Padre {
+	private int defectos;
+	private int virtudes;
+	
+	public Padre() {
+
+	}
+	
+	public int getDefectos() {
+		return defectos;
+	}
+	public void setDefectos(int defectos) {
+		this.defectos = defectos;
+	}
+	public int getVirtudes() {
+		return virtudes;
+	}
+	public void setVirtudes(int virtudes) {
+		this.virtudes = virtudes;
+	}
+	
+	public void imprimir() {
+		System.out.println("Virtudes: "+virtudes);
+		System.out.println("Defectos: "+defectos);
+	}
+	
+	public void guardarSecreto() {
+		System.out.println("Esto no se hereda");
+	}
+	
+	@Override
+	public String toString() {
+		return "Padre [defectos=" + defectos + ", virtudes=" + virtudes + "]";
+	}
+	
+	
+}
